b3a99522e5e32257e7b7c8908a9d3e8f0e5d005b
GuideLiu419 <160638709+GuideLiu419@users.noreply.github.com>
2024-08-26 19:54:00 +0800

    Add files via upload

--- 二叉树/BiTree.c
+++ 二叉树/BiTree.c
@@ -12,4 +12,39 @@ void PreOrderTraverse(BiTree T)
 	PreOrderTraverse(T->rchild);
 }
 
-//后序
+// 用递归法前序遍历创建二叉树 需要以‘#’表示空来补全二叉树
+Status CreatBiTree(BiTree* T)
+{
+	TElemType ch;
+	scanf("%c", & ch);
+
+	if (ch == '#')
+		*T == NULL;
+	else {
+		*T = (BiTree)malloc(sizeof(BiTNode));
+		if(*T == NULL)
+			return OVERFLOW;
+
+		(*T)->data = ch;//生成根节点
+
+		CreatBiTree(&(*T)->lchild);//生成左子树
+		CreatBiTree(&(*T)->rchild);//生成右子树
+	}
+}
+
+//递归计算二叉树深度
+int Depth(BiTree T)
+{
+	if (T == NULL)
+		return 0;
+	else
+	{
+		int m, n;
+		m = Depth(T->lchild);
+		n = Depth(T->rchild);
+		if (m > n)
+			return m + 1;
+		else
+			return n + 1;
+	}
+}
--- 二叉树/BiTree.h
+++ 二叉树/BiTree.h
@@ -15,12 +15,16 @@
 #define MAXSIZE 100 /* 存储空间初始分配量 */
 
 typedef int Status;		/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
-typedef char TElemType;
+typedef char TElemType; // 二叉树中存放的数据类型，此处设置为char
 typedef struct BiTNode  /* 结点结构 */
 {
-	TElemType data[10];		/* 结点数据 */
+	TElemType data;		/* 结点数据 */
 	struct BiTNode* lchild, * rchild; /* 左右孩子指针 */
 }BiTNode, * BiTree;
 
 //前序遍历递归算法
 void PreOrderTraverse(BiTree T);
+// 用递归法前序遍历创建二叉树 需要以‘#’表示空来补全二叉树
+Status CreatBiTree(BiTree* T);
+//递归计算二叉树深度
+int Depth(BiTree T);
